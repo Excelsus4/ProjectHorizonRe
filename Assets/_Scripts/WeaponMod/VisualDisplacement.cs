6073902f7254cb61e480ecf48797d1606efd74c0
Excelsus4 <elijahko4@gmail.com>
2020-02-23 05:46:21 +0900

    First buildup on new weapon system
    and also a branch study

---
+++ Assets/_Scripts/WeaponMod/VisualDisplacement.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.meiguofandian.weaponMod {
+	public class VisualDisplacement {
+		public WeaponMod.ModPart targetPart;
+		public Vector2 displacement;
+	}
+}
---
+++ Assets/_Scripts/WeaponMod/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.meiguofandian.weaponMod {
+	public class Weapon {
+		public List<WeaponMod> weaponModList = new List<WeaponMod>();
+		public Weapon CreateNewWeapon(WeaponMod receiver) {
+			// An object of weapon only get created by receiver.
+			if(receiver.m_requirementInformation[(int)WeaponMod.ReqInfo.Mainly] == (System.UInt64)WeaponMod.ModPart.Receiver) {
+				Weapon newWeapon = new Weapon();
+				newWeapon.weaponModList.Add(receiver);
+				return newWeapon;
+			} else
+				return null;
+		}
+	}
+}
