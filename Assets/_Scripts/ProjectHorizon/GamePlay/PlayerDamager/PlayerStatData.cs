f14f9fb2632518a0970e3dbed108699f372b77ed
Excelsus4 <elijahko4@gmail.com>
2020-04-02 10:21:15 +0900

    Player Damager Buildup

---
+++ Assets/_Scripts/ProjectHorizon/GamePlay/PlayerDamager/PlayerStatData.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using com.meiguofandian.Modules.SynchronizedSaver;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace com.meiguofandian.ProjectHorizon.GamePlay.PlayerDamager {
+	public class PlayerStatData : SynchronizedSave {
+		private static PlayerStatData singleton;
+
+		public static PlayerStatData getSingleton() {
+			if (singleton != null)
+				return singleton;
+			else
+				return createSingleton();
+		}
+
+		private static PlayerStatData createSingleton() {
+			singleton = new PlayerStatData();
+			return singleton;
+		}
+		
+		public PlayerBindStat playerData;
+
+		public PlayerStatData() {
+			saveID = "playerData";
+			SynchronizeSaveData(SynchronizeType.Load);
+		}
+
+		protected override void SaveItem(Stream stream) {
+			BinaryFormatter formatter = new BinaryFormatter();
+			formatter.Serialize(stream, playerData);
+		}
+
+		protected override void LoadItem(Stream stream) {
+			BinaryFormatter formatter = new BinaryFormatter();
+			playerData = formatter.Deserialize(stream) as PlayerBindStat;
+		}
+
+		protected override void LoadItem() {
+			playerData = new PlayerBindStat();
+		}
+	}
+}
