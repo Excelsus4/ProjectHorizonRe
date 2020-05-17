75a0f8036ac87e5c1aacd26e5ad9d29dc3b20e25
Excelsus4 <elijahko4@gmail.com>
2020-05-17 17:48:47 +0900

    Basic Buildup

---
+++ Assets/_Scripts/ProjectHorizon/GamePlay/LPlatformer/MapLiner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.meiguofandian.ProjectHorizon.GamePlay.LPlatformer {
+	public class MapLiner : MonoBehaviour {
+		public bool isTest;
+		public string testMapJSON;
+		public GameObject testObject;
+
+		// Start is called before the first frame update
+		void Start() {
+
+		}
+
+		// Update is called once per frame
+		void Update() {
+
+		}
+	}
+}
