96c01bfa84ab68110a217af59c706159f4c2f681
Excelsus4 <elijahko4@gmail.com>
2020-05-19 05:36:07 +0900

    Gravity Kick

--- Assets/_Scripts/ProjectHorizon/GamePlay/LPlatformer/Gravitational.cs
+++ Assets/_Scripts/ProjectHorizon/GamePlay/LPlatformer/Gravitational.cs
@@ -4,14 +4,49 @@ using UnityEngine;
 
 namespace com.meiguofandian.ProjectHorizon.GamePlay.LPlatformer {
 	public class Gravitational : MonoBehaviour {
-		// Start is called before the first frame update
-		void Start() {
+		public LayerMask FloorMask;
+		public float FootLength = 0.5f;
+		public bool IsGrounded { get; private set; }
+		public float gravity = 0.98f;
+		private Vector2 groundNormal;
+		private Vector2 velocity;
 
+		private void FixedUpdate() {
+			RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, Vector2.down, FootLength, FloorMask);
+			if (raycastHit.collider) {
+				transform.Translate(0f, (FootLength/2) - raycastHit.distance, 0f);
+				IsGrounded = true;
+				groundNormal = raycastHit.normal;
+			} else {
+				IsGrounded = false;
+			}
+
+			PerformTranslation();
 		}
 
-		// Update is called once per frame
-		void Update() {
+		private void PerformTranslation() {
+			if (!IsGrounded) {
+				velocity.y -= gravity;
+				if(velocity.y * Time.deltaTime < -FootLength) {
+					velocity.y = -FootLength / Time.deltaTime;
+				}
+			} else {
+				velocity.y = 0f;
+			}
 
+			print(velocity * Time.deltaTime);
+			transform.Translate(velocity*Time.deltaTime);
+		}
+
+		public void Move(float xVelocity) {
+			if (IsGrounded) {
+				velocity.x = xVelocity;
+			}
+		}
+
+		public void Jump(float Force) {
+			velocity += groundNormal * Force;
+			transform.Translate(0f, FootLength / 2, 0f);
 		}
 	}
 }
--- Assets/_Scripts/ProjectHorizon/GamePlay/LPlatformer/MapLiner.cs
+++ Assets/_Scripts/ProjectHorizon/GamePlay/LPlatformer/MapLiner.cs
@@ -5,12 +5,27 @@ using UnityEngine;
 namespace com.meiguofandian.ProjectHorizon.GamePlay.LPlatformer {
 	public class MapLiner : MonoBehaviour {
 		public bool isTest;
-		public string testMapJSON;
-		public GameObject testObject;
+		public MapData testMapData;
+		public GameObject testLiners;
+		public Material[] testMaterials = new Material[MapData.COMPONENT_TYPE_SIZE];
 
 		// Start is called before the first frame update
 		void Start() {
+			if (isTest) {
+				foreach(MapData.MapComponent component in testMapData.Components) {
+					GameObject instantiatedObject = Instantiate(testLiners, transform);
+					LineRenderer lr = instantiatedObject.GetComponent<LineRenderer>();
+					EdgeCollider2D ec = instantiatedObject.GetComponent<EdgeCollider2D>();
 
+					lr.material = testMaterials[(int)component.CType];
+					lr.positionCount =component.Points.Length;
+					ec.points = component.Points;
+
+					for(int idx = 0; idx < component.Points.Length; idx++) {
+						lr.SetPosition(idx, component.Points[idx]);
+					}
+				}
+			}
 		}
 
 		// Update is called once per frame
