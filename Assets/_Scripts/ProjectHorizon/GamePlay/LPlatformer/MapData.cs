de4925a12504302acc1a6fa76ad91ed4f1a08786
Excelsus4 <elijahko4@gmail.com>
2020-06-02 01:03:37 +0900

    Trigger Buildup

--- Assets/_Scripts/ProjectHorizon/GamePlay/LPlatformer/MapData.cs
+++ Assets/_Scripts/ProjectHorizon/GamePlay/LPlatformer/MapData.cs
@@ -7,6 +7,7 @@ namespace com.meiguofandian.ProjectHorizon.GamePlay.LPlatformer {
 	[CreateAssetMenu(fileName = "New Map", menuName = "ProjectHorizon/LPlatformer/Map")]
 	public class MapData:ScriptableObject {
 		public MapComponent[] Components;
+		public TriggerComponent[] Triggers;
 
 		public const int COMPONENT_TYPE_SIZE = 5; 
 		public enum ComponentType {
@@ -17,10 +18,22 @@ namespace com.meiguofandian.ProjectHorizon.GamePlay.LPlatformer {
 			LAVA		// WILL KILL THE PLAYER INSTANTLY
 		}
 
+		public const int TRIGGER_TYPE_SIZE = 2;
+		public enum TriggerType {
+			PLAYER_START,
+			HOSTILE_MOB
+		}
+
 		[Serializable]
 		public class MapComponent {
 			public ComponentType CType;
 			public Vector2[] Points;
+		}
+
+		[Serializable]
+		public class TriggerComponent {
+			public TriggerType TType;
+			public Vector2 Position;
 		}
 	}
 }
