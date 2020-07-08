8019ee8bc733bbd44543caeafb31e88d67720160
Excelsus4 <elijahko4@gmail.com>
2020-07-08 18:42:13 +0900

    A naive version of dialogue action added.
    Also solves #16, It seems that it was not working because the library was forgotten for coins.

---
+++ Assets/_Scripts/ProjectHorizon/GamePlay/LPlatformer/MapSystem/Actions/Dialogue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.meiguofandian.ProjectHorizon.GamePlay.LPlatformer.MapSystem.Actions {
+	[CreateAssetMenu(fileName = "New Action", menuName = "ProjectHorizon/LPlatformer/Action/Dialogue")]
+	public class Dialogue : ActionComponent {
+		public string CharacterName;
+		public string Speech;
+
+		public override void Activate() {
+			Debug.Log(CharacterName + " said : " + Speech);
+		}
+	}
+}
