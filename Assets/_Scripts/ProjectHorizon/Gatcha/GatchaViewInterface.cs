83e2fd65fde1d027c3deab12b7f306fed9ed19b0
Excelsus4 <elijahko4@gmail.com>
2020-07-16 09:25:01 +0900

    Now Starting to make GATCHA

---
+++ Assets/_Scripts/ProjectHorizon/Gatcha/GatchaViewInterface.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using com.meiguofandian.ProjectHorizon.WeaponNInventory;
+
+namespace com.meiguofandian.ProjectHorizon.Gatcha {
+	public interface GatchaViewInterface{
+		void DoGatcha(InventoryItem input, InventoryItem output);
+	}
+}
---
+++ Assets/_Scripts/ProjectHorizon/Gatcha/SimpleGatchaView.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using com.meiguofandian.ProjectHorizon.WeaponNInventory;
+using UnityEngine;
+
+namespace com.meiguofandian.ProjectHorizon.Gatcha {
+	public class SimpleGatchaView : MonoBehaviour, GatchaViewInterface {
+		private void Start() {
+			
+		}
+
+		public void DoGatcha(InventoryItem input, InventoryItem output) {
+		}
+
+		private void FoldTong() {
+
+		}
+	}
+}
