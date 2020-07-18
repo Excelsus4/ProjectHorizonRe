1831e241df1f84c88320f164771de44006f8b6ac
Excelsus4 <elijahko4@gmail.com>
2020-07-18 11:31:40 +0900

    Weapon n Inventory scripts reorganized

---
+++ Assets/_Scripts/ProjectHorizon/WeaponNInventory/UI/EquipmentOverlayManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using com.meiguofandian.Modules.ObserverPattern;
+
+namespace com.meiguofandian.ProjectHorizon.WeaponNInventory {
+	public class EquipmentOverlayManager : MonoBehaviour,IModIconButtonCallback, IDataUpdateCallback {
+		private List<ModIconRenderer> listOfIconRenderers = new List<ModIconRenderer>();
+
+
+
+		public GameObject m_IconObject;
+		public Transform m_IconParent;
+		public float m_IconSize;
+		public float m_IconGap;
+
+
+		public void ModIconCallback(int IDX, InventoryItem part) {
+			throw new System.NotImplementedException();
+		}
+
+		public void OnDataUpdate(string data) {
+			throw new System.NotImplementedException();
+		}
+	}
+}
