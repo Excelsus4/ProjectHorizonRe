61f3e03b6afaeb4b5745c006f9deb219eaa6f15b
Excelsus4 <elijahko4@gmail.com>
2020-07-31 10:48:14 +0900

    SpriteShader Implemented

---
+++ Assets/_Scripts/Modules/SpriteShaders/ChildRecolor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.meiguofandian.Modules.SpriteShaders {
+	public class ChildRecolor : MonoBehaviour {
+		public Material shader;
+
+		public void ApplyShader() {
+			SpriteRenderer[] spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+			foreach(SpriteRenderer spr in spriteRenderers) {
+				spr.sharedMaterial = shader;
+			}
+		}
+	}
+}
---
+++ Assets/_Scripts/Modules/SpriteShaders/Editor/ChildRecolorEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace com.meiguofandian.Modules.SpriteShaders {
+	[CustomEditor(typeof(ChildRecolor))]
+	public class ChildRecolorEditor : Editor {
+		public override void OnInspectorGUI() {
+			base.OnInspectorGUI();
+
+			ChildRecolor recolor = (ChildRecolor)target;
+			if (GUILayout.Button("Apply Shader")) {
+				recolor.ApplyShader();
+			}
+		}
+	}
+}
