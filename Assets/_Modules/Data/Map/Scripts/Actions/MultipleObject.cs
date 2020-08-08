c0a8d51db16dc822a3c899393702e241ca8ac89d
Excelsus4 <elijahko4@gmail.com>
2020-08-08 21:18:16 +0900

    Floating Crate Mob

---
+++ Assets/_Modules/Data/Map/Scripts/Actions/MultipleObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.meiguofandian.ProjectHorizon.GamePlay.LPlatformer.MapSystem.Actions {
+	[CreateAssetMenu(fileName = "New Action", menuName = "ProjectHorizon/LPlatformer/Action/MultipleObject")]
+	public class MultipleObject : ActionComponent {
+		public Vector2[] CertainPosition;
+		public GameObject CertainObject;
+
+		public override void Activate() {
+			foreach(Vector2 point in CertainPosition) {
+				Instantiate(CertainObject, point, Quaternion.identity);
+			}
+		}
+	}
+}
---
+++ Assets/_Modules/Gameplay/ShootableMob/Animator/FloatingAnimationManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.meiguofandian.ProjectHorizon.GamePlay.Shootables {
+	public class FloatingAnimationManager : MonoBehaviour, IMobAnimation {
+		public float BounceSpeed;
+		public float BounceAmount;
+
+		private float bounce;
+
+		private void Start() {
+			bounce = Random.Range(0f, BounceSpeed);
+		}
+
+		private void Update(){
+			bounce += Time.deltaTime;
+			float t = bounce % BounceSpeed / BounceSpeed * 2;
+			transform.localPosition = Vector3.up*Vector3.Slerp(Vector3.down * BounceAmount, Vector3.up * BounceAmount, t > 1 ? 2 - t : t).y;
+		}
+
+		public void Attacked() {
+		}
+
+		public void Death() {
+		}
+
+		public void StartMelee() {
+		}
+
+		public void StopMelee() {
+		}
+	}
+}
